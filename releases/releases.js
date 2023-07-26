bf08daa9112558f1749ebac426b71f225b94772e
snoopy <104867141+snoopyxz@users.noreply.github.com>
2023-07-26 12:45:24 +0100

    Add files via upload

---
+++ releases/releases.js
@@ -0,0 +1,57 @@
+
+var releaseCount = 5
+try {
+  var n = Math.max(1, parseInt(new URLSearchParams(window.location.search).get('n')))
+  if (n !== 0 && !Number.isNaN(n)) {
+    releaseCount = n
+  }
+} catch (e) {}
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  fetch('https://api.github.com/repos/minbrowser/min/releases').then(response => response.json())
+   .then(function (releases) {
+     var container = document.getElementById('releases')
+     releases
+     .slice(0, releaseCount)
+     .forEach(function (release, index) {
+       // display release info
+       var relContainer = document.createElement('div')
+       relContainer.className = 'release-container'
+
+       var heading = document.createElement('h2')
+       heading.textContent = release.name
+       relContainer.appendChild(heading)
+
+       var dateHeading = document.createElement('div')
+       dateHeading.className = 'release-date'
+       dateHeading.textContent = (new Intl.DateTimeFormat(navigator.language, {month: 'long', day: 'numeric', year: 'numeric'})).format(new Date(release.created_at))
+       relContainer.appendChild(dateHeading)
+
+       if (index === 0) {
+           // only show download button for latest release
+         var downloadLink = document.createElement('a')
+         var button = document.createElement('button')
+         button.className = 'button outlined-button outlined-button-white'
+         var buttonLabel = document.createElement('span')
+         buttonLabel.className = 'button-label'
+         buttonLabel.textContent = 'Download'
+         button.appendChild(buttonLabel)
+         downloadLink.appendChild(button)
+
+         setupDownloadButton(button)
+         relContainer.appendChild(downloadLink)
+
+         var spacer = document.createElement('div')
+         spacer.className = 'release-spacer'
+         relContainer.appendChild(spacer)
+       }
+
+       var notes = document.createElement('div')
+       notes.className = 'release-notes'
+       notes.innerHTML = new markdownit({html: true}).render(release.body)
+       relContainer.appendChild(notes)
+
+       container.appendChild(relContainer)
+     })
+   })
+})
